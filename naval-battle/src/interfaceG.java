aa706199c2cd64025f59936163b269da9d867855
Teddy <Teddy@Teddy-PC>
2015-12-23 11:50:12 +0100

    Refonte de l'interface graphique

--- naval-battle/src/interfaceG.java
+++ naval-battle/src/interfaceG.java
@@ -4,94 +4,151 @@ import java.awt.event.*;
 
 public class interfaceG extends JFrame implements ActionListener {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
-	private JButton Grille1[];
-	private JButton Grille2[];
-	private JPanel Plateaux;
-	private JPanel PlateauxJ1;
-	private JPanel PlateauxJ2;
+
+	//Déclaration des attributs pour l'interface joueur
 	private JPanel InterfaceJoueur;
 	private JPanel InterfaceJ1;
 	private JPanel InterfaceJ2;
 	private JLabel NomJ1;
 	private JLabel NomJ2;
+	private JLabel ValeurNomJ1;
+	private JLabel ValeurNomJ2;
 	private JLabel BateauxRestantJ1;
 	private JLabel BateauxRestantJ2;
+	private JLabel ValeurBateauxRestantJ1;
+	private JLabel ValeurBateauxRestantJ2;
+	
+	//Déclaration des attributs pour le plateaux de jeu
+	private JPanel Plateaux;
+	private JPanel PlateauxJ1;
+	private JPanel PlateauxJ2;
+	private JButton Grille1[];
+	private JButton Grille2[];
+	
+	//Déclaration des attributs pour les boutons systèmes du jeu
+	private JPanel Systeme;
+	private JButton BoutonStart;
+	private JButton BoutonStopRestart;
+	private JLabel ResultatScore;
+	private JLabel ValeurResultatScore;
+	private JLabel Vide1;
+	private JLabel Vide2;
+	private JLabel Vide3;
+	private JLabel Vide4;
 	
 	
 	
+	private static final long serialVersionUID = 1L;
+
 	
 	//constructeur interface graphique
 	public interfaceG()
 	{
+		//Appel du contructeur JFrame
 		super("Battleship");
 		
-		setSize(1200, 400);
-		this.setLocationRelativeTo(null);
-		Plateaux = new JPanel();
-		PlateauxJ1 = new JPanel();
-		PlateauxJ2 = new JPanel();
+		
+		//Contruction des éléments pour l'interface joueur
 		InterfaceJoueur = new JPanel();
 		InterfaceJ1 = new JPanel();
 		InterfaceJ2 = new JPanel();
+		NomJ1 = new JLabel("Nom du Joueur 1: ");
+		NomJ2 = new JLabel("Nom du Joueur 2: ");
+		ValeurNomJ1 = new JLabel("Joueur1");
+		ValeurNomJ2 = new JLabel("Joueur2");
+		BateauxRestantJ1 = new JLabel("Nombre de bateaux restants: ");
+		BateauxRestantJ2 = new JLabel("Nombre de bateaux restants: ");
+		ValeurBateauxRestantJ1 = new JLabel("1");
+		ValeurBateauxRestantJ2 = new JLabel("2");
+		
+		//Construction des éléments pour le plateaux de jeu
+		Plateaux = new JPanel();
+		PlateauxJ1 = new JPanel();
+		PlateauxJ2 = new JPanel();
 		Grille1 = new JButton[100];
 		Grille2 = new JButton[100];
-		NomJ1 = new JLabel("J1");
-		NomJ2 = new JLabel("J2");
-		BateauxRestantJ1 =new JLabel("Bateaux Restant: ");
-		BateauxRestantJ2 =new JLabel("Bateaux Restant: ");
-		//création de la zone de jeux avec les plateaux des joueurs
+		
+		//Construction des éléments pour les boutons système du jeu
+		BoutonStart = new JButton();
+		BoutonStart.setText("Start");
+		BoutonStopRestart = new JButton();
+		BoutonStopRestart.setText("Stop & Restart");
+		Systeme = new JPanel();
+		ResultatScore = new JLabel("Résultat de la partie: ");
+		ValeurResultatScore = new JLabel("Victoire Joueur 1");
+		Vide1 = new JLabel("");
+		Vide2 = new JLabel("");
+		Vide3 = new JLabel("");
+		Vide4 = new JLabel("");
+		
+		
+		// Construction de la frame
+		
+		setSize(1170, 500);
+		
+		setLocationRelativeTo(null);
+		
+		
+		//Organisation de la frame avec un BorderLayout
 		getContentPane().setLayout(new BorderLayout());
-		getContentPane().add(Plateaux, BorderLayout.CENTER);
 		
-		Plateaux.setLayout(new GridLayout( 1, 2, 40, 0));
-		Plateaux.add(PlateauxJ1);
-		Plateaux.add(PlateauxJ2);
-		PlateauxJ1.setLayout(new GridLayout(10, 10));
-		PlateauxJ2.setLayout(new GridLayout(10, 10));
-		
-		
-		//Bar horizontale des joueurs
+		//Construction de l'interface status des joueurs (north du borderlayout)
 		getContentPane().add(InterfaceJoueur, BorderLayout.NORTH);
-		InterfaceJoueur.setLayout(new GridLayout(1, 2, 100, 50));
+		InterfaceJoueur.setLayout(new GridLayout( 1, 2, 35, 0));
 		InterfaceJoueur.add(InterfaceJ1);
 		InterfaceJoueur.add(InterfaceJ2);
-		InterfaceJ1.setLayout(new GridLayout(3, 1, 0, 5));
-		InterfaceJ2.setLayout(new GridLayout(3, 1, 0, 5));
+		InterfaceJ1.setLayout(new GridLayout(2, 2, 0, 10));
+		InterfaceJ2.setLayout(new GridLayout(2, 2, 0, 10));
 		InterfaceJ1.add(NomJ1);
+		InterfaceJ1.add(ValeurNomJ1);
 		InterfaceJ1.add(BateauxRestantJ1);
+		InterfaceJ1.add(ValeurBateauxRestantJ1);
 		InterfaceJ2.add(NomJ2);
+		InterfaceJ2.add(ValeurNomJ2);
 		InterfaceJ2.add(BateauxRestantJ2);
-
+		InterfaceJ2.add(ValeurBateauxRestantJ2);
 		
-		
-		
-		
-		
+		//Construction du plateaux avec la grille des joueurs
+		getContentPane().add(Plateaux, BorderLayout.CENTER);
+		Plateaux.setLayout(new GridLayout(1, 2, 30, 30));
+		Plateaux.add(PlateauxJ1);
+		Plateaux.add(PlateauxJ2);
+		PlateauxJ1.setLayout(new GridLayout(10, 10, 0, 0));
+		PlateauxJ2.setLayout(new GridLayout(10, 10, 0, 0));
 		
 		for(int i = 0; i <= 99; i++)
 		{
 			Grille1[i] = new JButton(""+(i+1));
 			Grille1[i].setActionCommand("Action"+i);
 			Grille1[i].addActionListener(this);
-			//Grille1[i].setPreferredSize(new Dimension(1,1));
 			PlateauxJ1.add(Grille1[i]);
-			//Grille1[i].setPreferredSize(Taille);
-			Grille1[i].setBounds(1, 1, 10, 10);
+
 		}
+		
 		for(int i = 0; i <= 99; i++)
 		{
 			Grille2[i] = new JButton(""+(i+1));
 			Grille2[i].setActionCommand("Action"+i);
 			Grille2[i].addActionListener(this);
 			PlateauxJ2.add(Grille2[i]);
-			//Grille2[i].setPreferredSize(Taille);
-		}
 
+		}
+		
 		setVisible(true);
+		
+		
+		//construction des boutons système du jeu
+		getContentPane().add(Systeme, BorderLayout.SOUTH);
+		Systeme.setLayout(new GridLayout(2, 4, 0, 0));
+		Systeme.add(Vide1);
+		Systeme.add(ResultatScore);
+		Systeme.add(ValeurResultatScore);
+		Systeme.add(Vide2);
+		Systeme.add(Vide3);
+		Systeme.add(BoutonStart);
+		Systeme.add(BoutonStopRestart);
+		Systeme.add(Vide4);
+		
 		
 		
 		
